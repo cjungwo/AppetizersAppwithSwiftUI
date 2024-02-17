e38f34ca7fa4c2c523fae50a7b57aa73ab2dde21
cjungwo <wjd516@gmail.com>
2024-02-17 18:28:13 +1100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by JungWoo Choi on 17/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
